d5e6e29f597a8258532eaf679203a12e55e52493
Ubuntu <ubuntu@ip-172-31-6-79.ap-southeast-1.compute.internal>
2014-12-22 14:23:03 +0000

    Kopispace changes

--- config/env/development.js
+++ config/env/development.js
@@ -7,7 +7,7 @@ module.exports = {
     debug: false
   },
   app: {
-    name: 'MEAN - FullStack JS - Development'
+    name: 'KOPISPACE - Where Ideas Brew^'
   },
   facebook: {
     clientID: 'DEFAULT_APP_ID',
--- config/env/production.js
+++ config/env/production.js
@@ -27,7 +27,7 @@ module.exports = {
     */
   },
   app: {
-    name: 'MEAN - A Modern Stack - Production'
+    name: 'KOPISPACE - Where Ideas Brew - Production'
   },
   facebook: {
     clientID: 'APP_ID',
